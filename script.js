781512dfcba9088d6381999b67b929074736a31d
negisuraj080 <143009358+negisuraj080@users.noreply.github.com>
2023-12-25 22:38:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+var isProceeding = confirm("Do you want to proceed?");
+
+    // Check the user's choice
+    if (isProceeding) {
+        // User clicked OK
+        alert("You clicked OK. Proceeding...");
+    } else {
+        // User clicked Cancel
+        alert("You clicked Cancel. Exiting...");
